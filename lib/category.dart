8a946788b663c16c97690252bb125023d07ba882
SABDAR SHAIK <developer.sabdar@gmail.com>
2018-09-18 08:02:17 +0530

    02 Category Widget Created

---
+++ lib/category.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Category extends StatelessWidget {
+
+  const Category();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("hello"),
+    );
+  }
+}
