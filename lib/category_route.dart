d9e7a769674f2f28f80ae3512aa895d87aeaedb9
SABDAR SHAIK <developer.sabdar@gmail.com>
2018-09-18 08:48:30 +0530

    03 Category Route Completed

--- lib/category_route.dart
+++ lib/category_route.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_udacity_course/category.dart';
+
+final _backgroundColor = Colors.green[100];
 
 class CategoryRoute extends StatelessWidget {
   const CategoryRoute();
@@ -24,10 +27,42 @@ class CategoryRoute extends StatelessWidget {
     Colors.red
   ];
 
+  Widget _buildCategoryWidgets(List<Widget> categories) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) => categories[index],
+      itemCount: categories.length,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final listView = Container();
-    final appBar = AppBar();
+    final categories = <Category>[];
+
+    for (var i = 0; i < _categoryNames.length; i++) {
+      categories.add(Category(
+        name: _categoryNames[i],
+        color: _baseColors[i],
+        iconLocation: Icons.cake,
+      ));
+    }
+
+    final listView = Container(
+      color: _backgroundColor,
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: _buildCategoryWidgets(categories),
+    );
+    final appBar = AppBar(
+      title: Text(
+        "Unit Converter",
+        style: TextStyle(
+          fontSize: 30.0,
+          color: Colors.black,
+        ),
+      ),
+      elevation: 0.0,
+      centerTitle: true,
+      backgroundColor: _backgroundColor,
+    );
     return Scaffold(
       appBar: appBar,
       body: listView,
