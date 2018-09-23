eac872e4bf02239bef9838bd5aea2d0e8e351950
SABDAR SHAIK <developer.sabdar@gmail.com>
2018-09-23 14:14:42 +0530

    07 backdrop completed

--- lib/category.dart
+++ lib/category.dart
@@ -1,87 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_udacity_course/converter_route.dart';
 import 'package:flutter_udacity_course/unit.dart';
 import 'package:meta/meta.dart';
 
-final _rowHeight = 100.0;
-final _borderRadius = BorderRadius.circular(_rowHeight / 2);
-
-class Category extends StatelessWidget {
+class Category {
   final String name;
   final ColorSwatch color;
   final IconData iconLocation;
   final List<Unit> units;
 
-  const Category(
-      {Key key,
-      @required this.name,
-      @required this.color,
-      @required this.iconLocation,
-      @required this.units})
-      : assert(name != null),
+  const Category({
+    @required this.name,
+    @required this.color,
+    @required this.iconLocation,
+    @required this.units,
+  })  : assert(name != null),
         assert(color != null),
         assert(iconLocation != null),
-        assert(units != null),
-        super(key: key);
+        assert(units != null);
 
-  void _navigateToConverter(BuildContext context) {
-    Navigator.of(context)
-        .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          elevation: 1.0,
-          title: Text(
-            name,
-            style: Theme.of(context).textTheme.display1,
-          ),
-          centerTitle: true,
-          backgroundColor: color,
-        ),
-        body: ConverterRoute(
-          color: color,
-          name: name,
-          units: units,
-        ),
-        resizeToAvoidBottomPadding: false,
-      );
-    }));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Colors.transparent,
-      child: Container(
-        height: _rowHeight,
-        child: InkWell(
-          borderRadius: _borderRadius,
-          highlightColor: color['highlight'],
-          splashColor: color['splash'],
-          onTap: () => _navigateToConverter(context),
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Icon(
-                    iconLocation,
-                    size: 60.0,
-                  ),
-                ),
-                Center(
-                  child: Text(
-                    name,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.headline,
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+ 
 }
